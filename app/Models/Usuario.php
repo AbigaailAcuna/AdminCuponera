6b77395bac257a413f25511c4cdc27e5d6d1ac4b
lupitasgm <93928511+lupitasgm@users.noreply.github.com>
2023-05-10 15:08:32 -0600

    Add files via upload

---
+++ app/Models/Usuario.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Usuario
+ * 
+ * @property int $Id
+ * @property int $rol
+ * @property string $Email
+ * @property string $Password
+ * 
+ * @package App\Models
+ */
+
+class Usuario extends Model
+{
+    protected $table = 'usuarios';
+    protected $primaryKey = 'Id';
+    public $incrementing = false;
+	public $timestamps = false;
+
+    protected $casts = [
+		'Rol' => 'int'
+	];
+    protected $fillable = [
+		'Rol',
+		'Email',
+		'Password'
+	];
+
+}
