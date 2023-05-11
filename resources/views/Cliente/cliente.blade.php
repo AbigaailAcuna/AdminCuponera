4a8fe1d8b4dbf77edcca910d21871b6a983c1021
AbigaailAcuna <acuaabigail@yahoo.com>
2023-05-10 20:02:59 -0600

    Mostrando datos de sesion en vista clientes

--- resources/views/Cliente/cliente.blade.php
+++ resources/views/Cliente/cliente.blade.php
@@ -81,7 +81,7 @@
                 </div>
                 <div class="sb-sidenav-footer">
                     <div class="small">Logged in as:</div>
-                    Start Bootstrap
+                    {{ session('user')->Email }} 
                 </div>
             </nav>
         </div>
